6033a0930ae9e791b0c0227f10126b356627c97c
Guillermina Cledou <guillecledou@gmail.com>
2022-03-10 12:22:34 +0000

    cleaning fork join context information

--- src/main/scala/choreo/gen/NPom2SessionCtx.scala
+++ src/main/scala/choreo/gen/NPom2SessionCtx.scala
@@ -50,16 +50,6 @@ object NPom2SessionCtx:
     val post  = pre++mkPostEvid(e,pom) // pre because no we don't have explicit the variable names
     EventCtx(e,pom.actions(e).asInstanceOf[In|Out],pre,post,"v"+e,TVar("V"+e),"true")
 
-//  protected def getForkJoinInfo(pom:NPomset):Option[ForkJoin] =
-//    val initials = pom.minimum()
-//    if initials.size > 1 then
-//      val fork                = Fork(Many(initials),initials.map(i=>i->List(i)).toMap)
-//      val (forks,joins,lasts) = traverse(fork::Nil)(pom)
-//      val mergedJoins         = mergeJoins(joins)
-//      val lastJoin            = getLastJoin(lasts)
-//      Some(mkForkJoinInfo(forks,mergedJoins++lastJoin.toList)(pom))
-//    else traverseShard(initials.head)(pom)
-
   protected def mkForkJoinInfo(forks:List[Fork],joins:List[Join])(implicit pom:NPomset):ForkJoin =
     val lastJoin = getLastJoin(joins)
     val forkInfo = for f <- forks yield mkForkInfo(f)
@@ -113,25 +103,13 @@ object NPom2SessionCtx:
       visit -= next
     last
 
-
-
-//  protected def mergeJoins(joins:List[Join]):List[Join] =
-//    val joinsByPoint = joins.groupBy(j=>j.point)
-//    for (jp,ji) <- joinsByPoint.toList yield
-//      Join(jp,ji.flatMap(j=>j.regions))
-//
-//  protected def getLastJoin(lasts: List[(Event, Int)]):Option[Join] =
-//    val lastsByEvent = lasts.groupBy(_._1)
-//    if lastsByEvent.size <= 1 then None
-//    else Some(Join(Many(lasts.map(_._1).toSet),lasts.map(_._1).distinct))
-
   case class ForkInfo(point: Split, pre: Event2Value, post: List[RegionInfo])
   case class JoinInfo(point:Split, regions:List[Int], post:Event2Value)
   case class RegionInfo(id:Int,values:Event2Value)
 
   /**
    * Information about forks
- *
+   *
    * @param forks a map from each fork point to its branching information
    * @param joins a map from each join point and its merging information
    */
@@ -142,112 +120,6 @@ object NPom2SessionCtx:
          |joins:\n ${joins.mkString("\n")}
          |""".stripMargin
 
-//  protected def traverseShard(e:Event)(implicit pom:NPomset):Option[ForkJoin] =
-//    val succ = pom.succ.getOrElse(e,Set())
-//    if succ.isEmpty then
-//      None
-//    else if succ.size > 1 then
-//      val fork = Fork(One(e),succ.map(s=>s->List(s)).toMap)
-//      val (forks,joins,lasts) = traverse(fork::Nil,)
-//      val mergedJoins         = mergeJoins(joins)
-//      val lastJoin            = getLastJoin(lasts)
-//      Some(mkForkJoinInfo(forks,mergedJoins++lastJoin.toList))
-//    else traverseShard(succ.head)
-//
-//  protected def traverse(forks:List[Fork],visited:Set[Event])(implicit pom:NPomset)
-//    :(List[Fork],List[Join],List[(Event,Int)],Set[Event]) =
-//    var traversedForks = List[Fork]()
-//    var toTraverse = List[Fork]()
-//    var njoins = List[Join]()
-//    var lasts = List[(Event,Int)]()
-//    var allVisited = visited
-//    for f <- forks do
-//      val (nf,mf,mj,l,nvisited) = traverseFork(f,allVisited)
-//      traversedForks :+= nf
-//      toTraverse ++= mf
-//      njoins ++=mj
-//      lasts ++= l
-//      allVisited ++=nvisited
-//    if toTraverse.isEmpty then
-//      (traversedForks,njoins,lasts)
-//    else
-//      val (fs,js,ls,nvisited) = traverse(toTraverse,allVisited)
-//      (traversedForks++fs,njoins++js,lasts++ls,nvisited)
-//
-//  protected def traverseFork(fork: Fork,visited:Set[Event])(implicit pomset: NPomset)
-//    :(Fork,List[Fork],List[Join],List[(Event,Int)]) =
-//    var nforks = List[Fork]()
-//    var njoins = List[Join]()
-//    var lasts = List[(Event,Int)]()
-//    var nfork = fork
-//    var allVisited = visited
-//    for (r,b) <- fork.branches do
-//      val (nf,nfs,njs,l,nvisited) = traverseBranch(r,nfork,visited)
-//      nfork = nf
-//      if nfs.isDefined then nforks :+= nfs.get
-//      if njs.isDefined then njoins :+= njs.get
-//      if l.isDefined  then lasts  :+= l.get
-//      allVisited++=nvisited
-//
-//    (nfork,nforks,njoins,lasts,allVisited)
-//
-//  protected def traverseBranch(r:Int,fork:Fork,visited:Set[Event])(implicit pom: NPomset)
-//    :(Fork,Option[Fork],Option[Join],Option[(Event,Int)],Set[Event]) =
-//    // starting to traverse the new branch
-//    val succ = pom.succ.getOrElse(r,Set())
-//    if succ.isEmpty then // r is the first and last element in the shard
-//      (fork,None,None,Some((r,r)),visited+r)
-//    else if succ.size > 1 then // r it's the 1st element and also a fork point
-//      (fork,Some(Fork(One(r), succ.map(s=>s->List(s)).toMap)), None, None,visited+r)
-//    else // exactly on successor, then traverse the branch
-//      traverseBranch(r,fork,succ.head,visited)
-//
-//  protected def traverseBranch(r:Int,fork:Fork,e:Event,visited:Set[Event])(implicit pom:NPomset)
-//    : (Fork,Option[Fork],Option[Join],Option[(Event,Int)],Set[Event]) =
-//    if visited.contains(e) then
-//      (fork,None,None,None,visited)
-//    else
-//      val pred = pom.realPred(e)
-//      val succ = pom.succ.getOrElse(e,Set())
-//      if succ.isEmpty then // e has no succesors
-//        val pred = pom.realPred(e)
-//        if pred.size <= 1 then // additionally, it doesn't join any branch
-//          // last element in the region and a part of a special join
-//          (fork.addEvent2Region(e,r),None,None,Some((e,r)),visited+e)
-//        else // pred > 1, so is a join with no succesors
-//          (fork,None,Some(Join(One(e), List(r))),Some((e,r)),visited+e)//Map(fork.branches(r).last,r)),None)
-//      else if succ.size > 1 then // fork point
-//        val pred = pom.realPred(e)
-//        if pred.size <= 1 then // no join
-//          (fork,Some(Fork(One(e),succ.map(s=>s->List(s)).toMap)),None,None,visited+e)
-//        else // also a join
-//          (fork,None,Some(Join(One(e), List(r))),None,visited+e)
-//      else // has exactly one successor, it is part of the shard
-//        traverseBranch(r,fork.addEvent2Region(e,r),succ.head,visited+e)
-//
-//  protected def traverseBranch1(r:Int,fork:Fork,e:Event,visited:Set[Event])(implicit pom:NPomset)
-//  : (Fork,Option[Fork],Option[Join],Option[(Event,Int)],Set[Event]) =
-//    if visited.contains(e) then
-//      (fork,None,None,None,visited)
-//    else
-//      val pred = pom.realPred(e)
-//      val succ = pom.succ.getOrElse(e,Set())
-//      val mbFork =
-//        if succ.size > 1 then // is a fork
-//          Some(Fork(One(e),succ.map(s=>s->List(s)).toMap))
-//        else None
-//      val mbJoin =
-//        if pred.size > 1 then // is a join
-//          Some(Join(One(e), List(r)))
-//        else None
-//      val mbLast =
-//        if succ.isEmpty then // last element
-//          Some((e,r))
-//        else None
-//      if mbFork.isDefined || mbJoin.isDefined || mbLast.isDefined then
-//        (fork,mbFork,mbJoin,mbLast,visited+e)
-//      else // has exactly one successor and one predecessor, it is part of the shard
-//        traverseBranch(r,fork.addEvent2Region(e,r),succ.head,visited+e)
 
   case class TraverseInfo(
     forks:Map[Event,Fork],
@@ -347,11 +219,6 @@ object NPom2SessionCtx:
       traversInfo.addJoin(e,Join(One(e),List(r)))
     else traversInfo
 
-
-  //  val specialInit = -1
-//  val specialLast = -2
-  type Forks = List[Fork]
-  type Joins = List[Join]
   sealed trait Split
   case class One(e:Event)         extends Split
   case class Many(events:Set[Event]) extends Split
@@ -376,9 +243,3 @@ object NPom2SessionCtx:
   protected def optionClassName(a:Agent,i:Int): String = a.s.toUpperCase + "$Pom" +i.toString
   protected def agentClassName(a:Agent): String = a.s.toUpperCase + "$State"
 
-
-//  protected def sharedActions(poms:Iterable[NPomset]):Set[Action] =
-//    val actionsPerPom = poms.map(p=>p.actions.values.toSet)
-//    if actionsPerPom.size > 1 then
-//      actionsPerPom.tail.foldRight(actionsPerPom.head)(_.intersect(_))
-//    else Set()
